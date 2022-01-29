b3efed69ee2026460c2217bac858b05e0bb70ae5
Leigh Pfeiffer <leigh.pfeiffer@gmail.com>
2022-01-28 23:01:22 -0500

    button click

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+console.dir(window.document);
+
+//Button
+var buttonE1 = document.querySelector("#save-task");
+console.log(buttonE1);
+
+buttonEl.addEventListener("click", function() {
+    alert("button clicked");
+  });
+  
